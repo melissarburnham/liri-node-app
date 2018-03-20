bd4f032000668ed972e7f412874ed69716a2e60a
Your Name <melissarburnham@gmail.com>
2018-03-20 14:18:29 -0400

    attempt twitter call

--- liri.js
+++ liri.js
@@ -31,54 +31,40 @@ switch (arg2) {
    
 // }
 
+function myTweets(){
+    var queryUrl = "https://api.twitter.com/1.1/search/tweets.json?q="+ keys.twitter.consumer_key + "&count=20"
+    request(queryUrl, function(error, response, body) {
 
-
-
-
-// function myTweets(){
-//     console.log(keys.twitter);
-//     fs.appendFile("log.txt", keys.twitter, function(err) {
-//         if (err) {
-//           console.log(err);
-//         }
-//         else {
-//           console.log("Content added to log.txt!");
-//         }
-//     });
-// }
+      if (!error && response.statusCode === 200) {
+      //   console.log(JSON.parse(body));
+        var tweets = JSON.parse(body);
+        console.log(tweets);
+          
+      }
+    });
+  }
 
 
 function movieThis(){
-var queryUrl = "http://www.omdbapi.com/?t=" + arg3 + "&y=&plot=short&apikey=trilogy";
+    //need help: rotten tomato rating, making code dryer, and appending to log.txt
+
+var queryUrl = "http://www.omdbapi.com/?t=" + arg2  + "&y=&plot=short&apikey=trilogy";
 request(queryUrl, function(error, response, body) {
 
     if (!error && response.statusCode === 200) {
     //   console.log(JSON.parse(body));
+      var movie = JSON.parse(body);
       console.log(JSON.parse(body).Title);
-      console.log("The movie's release year is: " + JSON.parse(body).Year);
-      console.log("The movie's IMDB rating is: " + JSON.parse(body).imdbRating);
-      console.log("The movie's Rotten Tomato rating is: " + JSON.parse(body).Ratings.Source);
-      console.log("The movie's country is: " + JSON.parse(body).Country);
-      console.log("The movie's language is: " + JSON.parse(body).Language);       
-      console.log("The movie's plot is: " + JSON.parse(body).Plot);
-      console.log("The movie's actors are: " + JSON.parse(body).Actors);    
+      console.log("The movie's release year is: " + movie.Year);
+      console.log("The movie's IMDB rating is: " + movie.imdbRating);
+      console.log("The movie's Rotten Tomato rating is: " + movie.Ratings.Source);
+      console.log("The movie's country is: " + movie.Country);
+      console.log("The movie's language is: " + movie.Language);       
+      console.log("The movie's plot is: " + movie.Plot);
+      console.log("The movie's actors are: " + movie.Actors);    
     }
   });
 }
-
-
-//    * Title of the movie.
-//    * Year the movie came out.
-//    * IMDB Rating of the movie.
-//    * Rotten Tomatoes Rating of the movie.
-//    * Country where the movie was produced.
-//    * Language of the movie.
-//    * Plot of the movie.
-//    * Actors in the movie.
-
-
-
-
 
 //Make it so liri.js can take in one of the following commands:
 // * `my-tweets`
@@ -96,3 +82,12 @@ request(queryUrl, function(error, response, body) {
 // ├── package-lock.json
 // ├── package.json
 // └── random.txt
+
+// fs.appendFile("log.txt", keys.twitter, function(err) {
+//     if (err) {
+//       console.log(err);
+//     }
+//     else {
+//       console.log("Content added to log.txt!");
+//     }
+// });
